f25e6ac70a490e3446b9ec520e51dd7c9c770876
Hashir-Durrani <muhammad.hashir.durrani@ocsbstudent.ca>
2021-03-17 17:02:45 +0000

    Added notes to Java Script file.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.getElementById('submit').addEventListener('click', results)
+/*This line grabs the button element with id:submit from the HTML file and the event listener prevents any function from being applied until the button has been clicked.*/
 
 function results () {
   alert(document.getElementById('box').value)
 }
+/*This line in between the curly brackets is the function that will be applied to the event listener named "results".*/
